a6ed157d25754c43c08469aee8899b7db2a64b18
Madscool <madisonlorencole@gmail.com>
2024-11-25 19:23:55 -0500

    Create hb_order.js

---
+++ hb_order.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/* Defines arrays of item IDs, quantities, costs, and descriptions */
+
+var item = [1, 2, 3, 4, 5];
+
+var itemDescription = [ 'Double Chocolate Cake', 'French Vanilla Cupcake',
+  'Strawberry Mochi Cookie', 'Matcha Cupcake', 'Chocolate Marble Cake'];
+
+var itemPrice = [29.99, 9.99, 7.99, 9.99, 29.99];
+
+var itemQty = [1, 1, 1, 1];
